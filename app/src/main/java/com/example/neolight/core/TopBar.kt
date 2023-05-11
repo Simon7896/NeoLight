6ffa3e1f975421d3f5973e306de1857d970671fd
Simon Yu <simonyu180@gmail.com>
2023-05-11 15:54:49 -0700

    Refactored top app bar to seperate file

---
+++ app/src/main/java/com/example/neolight/core/TopBar.kt
@@ -0,0 +1,46 @@
+package com.example.neolight.core
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.neolight.R
+import com.example.neolight.ui.theme.NeoLightTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Menu, contentDescription = null)
+            }
+        },
+        actions = {
+            // RowScope here, so these icons will be placed horizontally
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Share, contentDescription = "Localized description")
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+    NeoLightTheme {
+        TopBar()
+    }
+}
